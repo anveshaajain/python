e9f548936a4191469584f9debc0f4273d954feff
anvesha jain <anveshajain1436@gmail.com>
2025-04-01 21:42:56 +0530

    Added permimeter file

---
+++ permimeter.py
@@ -0,0 +1,6 @@
+area=int(input("Enter the area of the rectangle: "))
+length=int(input("Enter the length of the rectangle: "))
+width=int(input("Enter the width of the rectangle: "))
+perimeter=2*(length+width)
+print("The perimeter of the rectangle is:", perimeter)
+# This code calculates the perimeter of a rectangle given its length and width.
