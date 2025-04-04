a8a9d7a0606b813ad89596ada3061ac987084234
anvesha jain <anveshajain1436@gmail.com>
2025-04-04 22:18:50 +0530

    Added module math file

---
+++ modulesmath.py
@@ -0,0 +1,17 @@
+
+import math
+print(math.floor(23.45)) 
+print(math.floor(-40.36))
+
+
+print(math.ceil(23.45))
+print(math.ceil(-40.36))
+
+print(math.pow(2, 3))
+print(math.pow(3, 2))
+
+print(math.sqrt(16))
+print(math.sqrt(25))
+
+print(math.fabs(-23.45))
+print(math.fabs(23.45))
